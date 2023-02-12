362feb24008f2ef848548390816f74bdcf8eb4d7
WestJeremy <116618595+WestJeremy@users.noreply.github.com>
2023-02-12 14:52:03 -0800

    SSAP now functions for 1 loop

--- Spiral2.py
+++ Spiral2.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import array
 import pandas as pd
 from scipy.signal import find_peaks
+from scipy.stats import linregress
 
 class Spiral:
     def __init__(self,x0:float,y0:float,points:float) -> None:
@@ -30,11 +31,35 @@ class Spiral:
         
         return x, y 
     
+class Linear: 
+    def __init__(self,data,points):
+        self.data=data #data
+        self.points=points #x offset
+        
+    def get(self,x): 
+        peaks= find_peaks(self.data['Intensity'], height=.1)
+    
+        H=peaks[1]['peak_heights']
+        A=[]
+        for i in range(len(IPeaks)):
+            corr=[IPeaks[i],H[i]]
+            A.append(corr)
+        SA=sorted(A, key=lambda x: x[1], reverse=True) #sort by peak height largest to smallest
+        XPP=[]
+        YPP=[]
+        for i in range(self.points):
+            #ICP.append(SA[i][0])
+            XPP.append(self.data['X'][SA[i][0]])
+            YPP.append(self.data['Y'][SA[i][0]])
+        LRR=linregress(XPP,YPP)
+        
+        return LRR[0]*x+LRR[1]
+    
 
 class Intensity: #intensity function is a gaussian curve 
     def __init__(self,x0:float,y0:float, mu_x, mu_y, sigma_x, sigma_y):
-        self.x0=x0 #x offset
-        self.y0=y0 #y offset
+        self.x0=-x0 #x offset
+        self.y0=-y0 #y offset
         self.mu_x=mu_x
         self.mu_y=mu_y
         self.sigma_x=sigma_x #x squish/stretch
@@ -46,15 +71,15 @@ class Intensity: #intensity function is a gaussian curve
         return I
 
         
-
+#*****************************************************************************************************************
 if __name__=="__main__":
     print('Calling spiral from class')
 
-    cur_spiral=Spiral(0,0,100) #setting params of the spiral we will be using
-    x,y=cur_spiral.build(3,.75) #build our spiral x y coords
-    
-    
-    TestInt=Intensity(.1,.2,0,0,2,1) #set test intensity parameters     
+    cur_spiral=Spiral(0,0,200) #setting params of the spiral we will be using
+    x,y=cur_spiral.build(3,1) #build our spiral x y coords
+    Ix=1.24
+    Iy=-2.3
+    TestInt=Intensity(Ix,Iy,0,0,2,1) #set test intensity parameters     
     
     #Plot our spiral
     plt.plot(x, y, color = 'red', marker = "o") 
@@ -81,18 +106,6 @@ titled_columns={'X': X,'Y': Y,'Z': Z,
                 'Intensity': Iv}
 data = pd.DataFrame(titled_columns)
 
-# dI=np.diff(data['Intensity'])
-# dI=np.insert(dI,0,-1)
-# data['dI']=dI
-
-# dp=np.array(np.sqrt(np.diff(data['X'])**2+(np.diff(data['Y'])**2))) #change in position from previous point
-# dp=np.insert(dp,0,1) #placeholder to make sizes match
-# data['dp']=dp
-
-# data['dI/dp']=data['dI']/data['dp']
-
-
-
 
 peaks= find_peaks(data['Intensity'], height=.1)
 IPeaks=peaks[0]
@@ -101,34 +114,53 @@ A=[]
 for i in range(len(IPeaks)):
     corr=[IPeaks[i],H[i]]
     A.append(corr)
+    
+ 
+    
 SA=sorted(A, key=lambda x: x[1], reverse=True) #sort by peak height largest to smallest
 
 
-#Plotting
+CL=Linear(data,3) #current linear line definition
+
+step=.01 #linear step size
+xl=data['X'][SA[0][0]] #start x
+yl=CL.get(xl) #start y
+
+Xl=[xl]  
+Yl=[yl]
+Il=[TestInt.get(xl,yl)]
+
+if  data['X'][SA[0][0]] < data['X'][SA[1][0]]: 
+    step=step
+else:
+    step=-step
+    
+    
+while TestInt.get(xl+step,CL.get(xl+step)) >= TestInt.get(xl,CL.get(xl)):
+    xl=xl+step
+    yl=CL.get(xl)
+    il=TestInt.get(xl,yl)
+    Xl.append(xl)
+    Yl.append(yl)
+    Il.append(il)
+
+
+LLdata={'X':Xl,'Y':Yl,'I':Il}
+Ldata=pd.DataFrame(LLdata)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot3D(data['X'], data['Y'], data['Intensity'])
 ax.plot3D(data['X'][IPeaks], data['Y'][IPeaks], data['Intensity'][IPeaks],"x")
+ax.plot3D(Xl, Yl, Il,'o')
 
 ax.set_xlabel('X axis')
 ax.set_ylabel('Y axis')
 ax.set_zlabel('Z axis')
-plt.show()
+plt.show()    
 
 
-# Ipks=data['Intensity'][peaks]
-
-# ii=np.sort(Ipks)
-
-
-
-#IpksS=Ipks[:,2].sort(reverse=True)
-m=max(data['Intensity'])
-print(np.where(data['Intensity'] == m)[0])
-#rint(ii)
-
-
-
-print(data)
-print(data['Intensity'][3])
-
+print('***Linear Translation********************************************************')
+print(Ldata)
+print('_________SSAP Report:_________')
+print('Calculated peak is at',xl,yl)
+print('True peak is at',Ix,Iy)
